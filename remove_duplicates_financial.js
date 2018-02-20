38ffb0cf659ca51c13b123027b078bf5e472e9e2
David Leonardi <davidleonardi418@gmail.com>
2018-02-20 20:40:25 +0800

    ADD: Add a list of charities with missing financial information.

--- remove_duplicates_financial.js
+++ remove_duplicates_financial.js
@@ -5,7 +5,7 @@ const Papa = require('papaparse');
 let outputData = [];
 let dict = {};
 
-const fileName = './data/final/charities_financial_information.csv';
+const fileName = './data/final/charities_financial_raw.csv';
 
 try {
     console.log(fileName);
@@ -22,13 +22,14 @@ try {
         if (dict[item['uen']] === undefined) {
             dict[item['uen']] = {};
         }
-        if (dict[item['uen']][item['index']] === undefined) {
+        if (dict[item['uen']][item['financial_period']] === undefined) {
+            item['financial_year'] = item['financial_period'].substr(4, 4) + '-' + item['financial_period'].substr(15, 4);
             outputData.push(item);
-            dict[item['uen']][item['index']] = 1;
+            dict[item['uen']][item['financial_period']] = 1;
         }
     });
 
-    writeFileSync(`./data/final/charities_financial_clean.csv`, Papa.unparse(outputData), {encoding: 'utf8'});
+    writeFileSync(`./data/final/charities_financial_information.csv`, Papa.unparse(outputData), {encoding: 'utf8'});
 
     console.log('Done');
 } catch (e) {
